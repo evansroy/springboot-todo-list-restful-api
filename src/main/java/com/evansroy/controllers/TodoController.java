b755c91b79c11c56b36b7ff3d3dd0dac04326925
evansroy <evansroysir@gmail.com>
2023-03-09 18:42:08 +0300

    Add api version

--- src/main/java/com/evansroy/controllers/TodoController.java
+++ src/main/java/com/evansroy/controllers/TodoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/todos")
+@RequestMapping("api/v1/todos")
 public class TodoController {
     private final TodoService service;
 
